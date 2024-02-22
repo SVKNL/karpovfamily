e2a234a19d0788dc6645c2f483514b16adae4e51
admin <admin@MacBook-Pro-admin.local>
2024-02-22 23:11:08 +0300

    png and jpg solution for get_image

--- everythingatonce/overtime/parsing0.py
+++ everythingatonce/overtime/parsing0.py
@@ -6,6 +6,13 @@ from icrawler.builtin import GoogleImageCrawler
 def get_image(word):
     if os.path.exists('/Users/admin/karpovfamily/everythingatonce/media/images/000001.jpg'):
             os.remove('/Users/admin/karpovfamily/everythingatonce/media/images/000001.jpg')
+    if os.path.exists('/Users/admin/karpovfamily/everythingatonce/media/images/000001.png'):
+            os.remove('/Users/admin/karpovfamily/everythingatonce/media/images/000001.png')        
+    
     num=1
     google=GoogleImageCrawler(storage={'root_dir':'/Users/admin/karpovfamily/everythingatonce/media/images'})
     google.crawl(keyword=word, max_num=num)
+    if os.path.exists('/Users/admin/karpovfamily/everythingatonce/media/images/000001.png'):
+        return 'png'
+    if os.path.exists('/Users/admin/karpovfamily/everythingatonce/media/images/000001.jpg'):
+        return 'jpg'
